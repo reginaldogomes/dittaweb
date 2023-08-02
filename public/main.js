db994dc6fef418bc36db0f6abaf79bd40459ff5d
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2023-08-02 15:04:00 -0300

    link

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 function createMenu() {
   return `
     <div class="menu">
-      <a href="#/page2" class="menu__item">
+      <a href="#/login" class="menu__item">
         <h3>Vendite</h3>
         <p>Funzioni per la gestione delle vendite, prelievo, fatturazione...</p>
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
@@ -92,7 +92,7 @@ function createMenu() {
     </div>
   `;
 }
-function createMenu2() {
+function createLogin() {
   return `
     <div class="menu">
       <h1>teste</h1>
@@ -102,8 +102,8 @@ function createMenu2() {
 
 // Exemplo de uso: 
 const menuHtml = createMenu();
-const menuHtml2 = createMenu2();
-console.log(menuHtml);
+const loginHtml = createLogin();
+// console.log(menuHtml);
 
 // Função para atualizar o conteúdo da página com base na URL
 function updateContent() {
@@ -112,8 +112,8 @@ function updateContent() {
 
   if (hash === '') {
     content.innerHTML = menuHtml;
-  } else if (hash === '#/page2') {
-    content.innerHTML = menuHtml2;
+  } else if (hash === '#/login') {
+    content.innerHTML = loginHtml;
   } else if (hash === '#/contact') {
     content.innerHTML = '<h2>Contato</h2>';
   } else {
